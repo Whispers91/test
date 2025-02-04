6901a71120441a10a76ba519df01300b4bc483cb
Whispers91 <jackiboy567@gmail.com>
2025-02-04 21:18:29 +0200

    Add files via upload
    problem solving 5 questions

---
+++ Problem Solving/Task/index.js
@@ -0,0 +1,40 @@
+// Question 1
+// function printArray(number){
+//     var newArray = [];
+//     for(var i = 1; i <= number; i++){
+//       newArray.push(i);
+//     }
+//     return newArray;
+// }
+
+// Question 2
+// function findIndex(arr, str) {
+// 	for (let i = 0; i < arr.length; i++) {
+// 		if (arr[i] === str) {
+// 			return i;
+// 		}
+// 	}
+// }
+
+// Question 3
+
+
+// Question 4
+// function getSumOfItems(arr) {
+// 	let sum = 0
+// 	for (let i = 0; i < arr.length; i ++) {
+// 		sum += arr[i]
+// 	}
+// 	return sum
+// }
+
+// Question 5
+// function addOddToN(n) {
+// 	let sum = 0;
+// 	for (let i = 1; i <= n; i++) {
+// 		if (i % 2 !== 0) {
+// 			sum += i;
+// 		}
+// 	}
+// 	return sum;
+// }
