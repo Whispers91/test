80c3656c6da3df45077e27cd6326face0b6ef236
Whispers91 <jackiboy567@gmail.com>
2025-02-07 03:27:19 +0200

    Add files via upload
    5 Questions problem solving (2nd)

---
+++ Problem Solving 2/index.js
@@ -0,0 +1,37 @@
+// Question 1
+// function triArea(base, height) {
+// 	var area = 0;
+// 	area = 0.5 * base * height
+// 	return area
+// }
+
+// Question 2
+// function addition(num) {
+// 	var result = 0
+// 	result = num + 1
+// 	return result
+// }
+
+// Question 3
+// function convert(minutes) {
+// 	var seconds = 0;
+// 	seconds = minutes * 60
+// 	return seconds
+// }
+
+// Question 4
+// function findPerimeter(length, width) {
+// 	var perimeter = 0;
+// 	perimeter = (length + width) * 2;
+// 	return perimeter;
+// }
+
+// Question 5
+// function points(twoPointers, threePointers) {
+// 	var num1, num2;
+// 	var result;
+// 	num1 = twoPointers * 2;
+// 	num2 = threePointers * 3;
+// 	result = num1 + num2;
+// 	return result;
+// }
